9b95990f7e66cad3e3126de5fd806df9880288ac
PimentaJoao <jppimentas@hotmail.com>
2021-05-13 01:00:44 -0300

    adding map benchmark

--- main_test.go
+++ main_test.go
@@ -2,21 +2,6 @@ package main
 
 import "testing"
 
-// "math/rand"
-// "time"
-
-// var workloads []int
-
-// const min int = 1
-// const max int = 7
-
-// func setup() {
-// 	rand.Seed(time.Now().UnixNano())
-// 	for i := 0; i < 100; i++ {
-// 		workloads = append(workloads, rand.Intn(max-min+1)+max)
-// 	}
-// }
-
 var workloads []int = []int{4, 2, 6, 4, 5, 6, 7, 2, 3, 4, 1, 5, 4, 3, 2, 6, 2, 5, 7, 1, 2, 3, 5, 1, 2}
 var worksize int = len(workloads)
 
@@ -29,5 +14,11 @@ func BenchmarkIf(b *testing.B) {
 func BenchmarkSwitch(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		daySelectorSwitch(workloads[i%worksize])
+	}
+}
+
+func BenchmarkMap(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		daySelectorMap(workloads[i%worksize])
 	}
 }
